8f964fb5b67961c99c9fa4355a60a886dd52d3a1
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2023-08-21 10:50:22 +0900

    [level 1] Title: 폰켓몬, Time: 11.34 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/lv1/1845. 폰켓몬/폰켓몬.java
@@ -1,23 +1,12 @@
-import java.util.HashMap;
-import java.util.Map;
+import java.util.HashSet;
 
 class Solution {
     public int solution(int[] nums) {
-        //총 N마리의 폰켓몬 중에서 N/2마리를 가져가도 좋음
-        //같은 종류의 폰켓몬은 같은 번호
-        //최대한 다양한 종류의 폰켓몬을 가지길 원함
-        int size = nums.length / 2;
-        Map<Integer, Integer> countMap = new HashMap<>();
-
+        HashSet<Integer> count = new HashSet<>();
         for (int num : nums) {
-            int count = countMap.getOrDefault(num, 0);
-            countMap.put(num, count + 1);
+            count.add(num);
         }
 
-        if (countMap.size() >= size) {
-            return size;
-        }
-
-        return countMap.size();
+        return Math.min(count.size(), nums.length / 2);
     }
 }
