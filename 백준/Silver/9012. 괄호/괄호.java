3dd60a6f8a0c2497a86c5aebafaeb87000eb8119
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-01-04 16:18:44 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14428 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,33 +1,43 @@
-import java.util.*;
+import java.io.*;
+import java.util.Stack;
 
 public class Main {
 
-    private static Stack<Character> stack;
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int n = sc.nextInt();
-        sc.nextLine();
-        for (int i = 0; i < n; i++) {
-            String str = sc.nextLine();
-            String result = "YES";
-            stack = new Stack<>();
-            for (char ch : str.toCharArray()) {
-                if (ch == '(') {
-                    stack.push(ch);
-                } else {
-                    if (stack.isEmpty() || stack.peek() != '(') {
-                        result = "NO";
-                        break;
-                    }
-                    stack.pop();
-                }
+    private static boolean isVPS(String target) {
+        Stack<String> stack = new Stack<>();
+        for (char c : target.toCharArray()) {
+            if (c == '(') {
+                stack.push(String.valueOf(c));
+                continue;
             }
-            if (!stack.isEmpty()) {
-                result = "NO";
+
+            if (stack.empty()) {
+                return false;
             }
-            System.out.println(result);
+
+            stack.pop();
         }
+
+        return stack.empty();
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder builder = new StringBuilder();
+
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+
+            String result = isVPS(str) ? "YES" : "NO";
+
+            builder.append(result).append("\n");
+        }
+
+        bw.write(builder.toString());
+        bw.flush();
+        bw.close();
     }
 }
