ddc6806376203a0c5cb606c60c61834c353db582
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-02 15:23:52 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.09 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,9 +1,7 @@
 class Solution {
     public int solution(int n) {
-        String temp = Integer.toString(n, 3);
-        StringBuilder builder = new StringBuilder();
-        builder.append(temp);
-        builder.reverse();
-        return Integer.parseInt(String.valueOf(builder), 3);
+        String str = Integer.toString(n, 3);
+        String reversed = new StringBuilder(str).reverse().toString();
+        return Integer.parseInt(reversed, 3);
     }
 }
--- 프로그래머스/1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935#) 
 
 ### 성능 요약
 
-메모리: 77.3 MB, 시간: 0.11 ms
+메모리: 77.3 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 2일 16:19:14
+2024년 2월 5일 15:23:49
 
 ### 문제 설명
 
