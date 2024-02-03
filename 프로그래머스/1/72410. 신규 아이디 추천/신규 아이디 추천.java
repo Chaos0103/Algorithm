388b0c5a40e60deb66c55985f375d3bc2d784967
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-03 16:37:49 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.16 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,38 +1,25 @@
 class Solution {
-
     public String solution(String new_id) {
-
-        //step 1
         new_id = new_id.toLowerCase();
 
-        //step 2
-        new_id = new_id.replaceAll("[^a-z0-9.\\-_]", "");
+        new_id = new_id.replaceAll("[^a-z0-9\\-_.]", "");
 
-        //step 3
-        new_id = new_id.replaceAll("\\.{2,}", ".");
+        new_id = new_id.replaceAll("[.]+", ".");
 
-        //step 4
-        new_id = new_id.replaceAll("^\\.+", "");
-        new_id = new_id.replaceAll("\\.+$", "");
 
-        //step 5
+        new_id = new_id.replaceAll("^[.]|[.]$", "");
+
         if (new_id.isEmpty()) {
             new_id = "a";
         }
 
-        //step 6
         if (new_id.length() > 15) {
             new_id = new_id.substring(0, 15);
-            new_id = new_id.replaceAll("^\\.+", "");
-            new_id = new_id.replaceAll("\\.+$", "");
+            new_id = new_id.replaceAll("^[.]|[.]$", "");
         }
 
-        //step 7
         if (new_id.length() < 3) {
-            char ch = new_id.charAt(new_id.length() - 1);
-            for (int i = 0; i < ch * (3 - new_id.length()); i++) {
-                new_id += ch;
-            }
+            new_id += new_id.substring(new_id.length() - 1).repeat(3 - new_id.length());
         }
 
         return new_id;
