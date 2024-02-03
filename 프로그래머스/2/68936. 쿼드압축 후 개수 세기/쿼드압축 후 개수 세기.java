1afa1b13e198d65f5968e41ef76382afb75af20e
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-03 17:42:44 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 3.65 ms, Memory: 93.4 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,40 +1,36 @@
 class Solution {
-    private static class Count {
-        public final int zero;
-        public final int one;
 
-        public Count(int zero, int one) {
-            this.zero = zero;
-            this.one = one;
-        }
-
-        public Count add(Count other) {
-            return new Count(zero + other.zero, one + other.one);
-        }
-    }
-
-    private Count count(int offsetX, int offsetY, int size, int[][] arr) {
-        int h = size / 2;
-        for (int x = offsetX; x < offsetX + size; x++) {
-            for (int y = offsetY; y < offsetY + size; y++) {
-                if (arr[y][x] != arr[offsetY][offsetX]) {
-                    return count(offsetX, offsetY, h, arr)
-                        .add(count(offsetX + h, offsetY, h, arr))
-                        .add(count(offsetX, offsetY + h, h, arr))
-                        .add(count(offsetX + h, offsetY + h, h, arr));
-                }
-            }
-        }
-
-        if (arr[offsetY][offsetX] == 1) {
-            return new Count(0, 1);
-        }
-
-        return new Count(1, 0);
-    }
+    private static int zeroCount = 0;
+    private static int oneCount = 0;
 
     public int[] solution(int[][] arr) {
-        Count count = count(0, 0, arr.length, arr);
-        return new int[]{count.zero, count.one};
+        tree(arr, 0, 0, arr.length);
+        return new int[]{zeroCount, oneCount};
+    }
+
+    private int tree(int[][] arr, int x, int y, int size) {
+        if (size == 1) {
+            if (arr[y][x] == 0) {
+                zeroCount++;
+            } else {
+                oneCount++;
+            }
+            return arr[y][x];
+        }
+
+        int a = tree(arr, x, y, size / 2);
+        int b = tree(arr, x + size / 2, y, size / 2);
+        int c = tree(arr, x, y + size / 2, size / 2);
+        int d = tree(arr, x + size / 2, y + size / 2, size / 2);
+
+        if (a == 0 && b == 0 && c == 0 && d == 0) {
+            zeroCount -= 3;
+            return 0;
+        } else if (a == 1 && b == 1 && c == 1 && d == 1) {
+            oneCount -= 3;
+            return 1;
+        }
+
+        return -1;
     }
 }
