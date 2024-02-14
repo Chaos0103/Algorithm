9779f9f29c82f59190c6a9114a417e35d6cca1b5
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-14 15:53:37 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.02 ms, Memory: 74.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int[] numbers) {
+        int result = 45;
+        for (int number : numbers) {
+            result -= number;
+        }
+        return result;
+    }
+}
