bd0a823dc91eb80e7dc0483f5a983cc9ca77457e
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-27 03:02:44 +0900

    [Silver I] Title: 스타트와 링크, Time: 916 ms, Memory: 106016 KB -BaekjoonHub

--- 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
+++ 백준/Silver/14889. 스타트와 링크/스타트와 링크.java
@@ -1,45 +1,69 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.util.*;
 import java.util.stream.IntStream;
 
 public class Main {
 
-    private static int n;
-    private static final List<List<Integer>> comb = new ArrayList<>();
+    //팀 구성 조합을 저장할 리스트
+    private static final List<Set<Integer>> comb = new ArrayList<>();
 
-    private static void comb(int[] arr, boolean[] visited, int start, int depth) {
-        if (depth == n / 2) {
-            List<Integer> temp = new ArrayList<>();
+    //팀 구성 조합을 생성하는 메서드
+    //arr: 팀 번호가 담긴 배열
+    //isVisited: 팀 번호 방문(사용) 여부를 확인하기 위한 배열
+    //start: 팀 구성 조합을 시작할 인덱스
+    //depth: 현재 조합된 팀 구성원 수
+    private static void comb(int[] arr, boolean[] isVisited, int start, int depth) {
+        //팀원 구성이 완료된 경우
+        if (depth == arr.length / 2) {
+            //팀 구성 리스트 생성
+            Set<Integer> temp = new HashSet<>();
             for (int i = 0; i < arr.length; i++) {
-                if (visited[i]) {
+                if (isVisited[i]) {
                     temp.add(i);
                 }
             }
+            //팀 구성 조합 리스트에 추가
             comb.add(temp);
             return;
         }
+
+        //다음 팀원 탐색
         for (int i = start; i < arr.length; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                comb(arr, visited, i + 1, depth + 1);
-                visited[i] = false;
+            if (!isVisited[i]) {
+                isVisited[i] = true;
+                comb(arr, isVisited, i + 1, depth + 1);
+                isVisited[i] = false;
             }
         }
+    }
+
+    //팀 구성원 능력치 차이를 구하는 메서드
+    private static int getDiffTeamPerformance(Set<Integer> data, int[][] performances) {
+        int startTeam = 0;
+        int linkTeam = 0;
+        for (int i = 0; i < performances.length; i++) {
+            for (int j = 0; j < performances[i].length; j++) {
+                if (data.contains(i) && data.contains(j)) {
+                    startTeam += performances[i][j];
+                } else if (!data.contains(i) && !data.contains(j)) {
+                    linkTeam += performances[i][j];
+                }
+            }
+        }
+        return Math.abs(startTeam - linkTeam);
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
-        int[][] abilityValues = new int[n][n];
+        int n = Integer.parseInt(br.readLine());
+        int[][] performances = new int[n][n];
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < n; j++) {
-                abilityValues[i][j] = Integer.parseInt(st.nextToken());
+                performances[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
@@ -47,19 +71,9 @@ public class Main {
         comb(arr, new boolean[n], 0, 0);
 
         int result = Integer.MAX_VALUE;
-        for (List<Integer> data : comb) {
-            int startTeam = 0;
-            int linkTeam = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    if (data.contains(i) && data.contains(j)) {
-                        startTeam += abilityValues[i][j];
-                    } else if (!data.contains(i) && !data.contains(j)) {
-                        linkTeam += abilityValues[i][j];
-                    }
-                }
-            }
-            result = Math.min(result, Math.abs(startTeam - linkTeam));
+        for (Set<Integer> data : comb) {
+            int diff = getDiffTeamPerformance(data, performances);
+            result = Math.min(result, diff);
         }
 
         System.out.println(result);
