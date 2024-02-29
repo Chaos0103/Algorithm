f090be47b043ba4d4e701b8651b6b18bde1de2ec
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-02-29 17:33:39 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+class Solution {
+    public int solution(int N, int number) {
+        if (N == number) {
+            return 1;
+        }
+
+        List<Set<Integer>> dp = new ArrayList<>();
+        for (int i = 0; i <= 8; i++) {
+            dp.add(new HashSet<>());
+        }
+
+        dp.get(1).add(N);
+        for (int i = 2; i <= 8; i++) {
+
+            String num = Integer.toString(N).repeat(i);
+            dp.get(i).add(Integer.parseInt(num));
+
+            for (int j = 1; j < i; j++) {
+                int k = i - j;
+                for (int num1 : dp.get(k)) {
+                    for (int num2 : dp.get(j)) {
+                        dp.get(i).add(num1 + num2);
+                        dp.get(i).add(num1 - num2);
+                        dp.get(i).add(num1 * num2);
+                        if (num2 != 0) {
+                            dp.get(i).add(num1 / num2);
+                        }
+                    }
+                }
+            }
+
+            if (dp.get(i).contains(number)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
