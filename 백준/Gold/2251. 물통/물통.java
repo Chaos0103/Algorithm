fbc15616a54753e9978e8dc6d545c8ff6f05fcc9
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-03-12 22:40:30 +0900

    [Gold V] Title: 물통, Time: 148 ms, Memory: 23336 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -4,37 +4,30 @@ import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    private static Set<Integer> solution(int a, int b, int c) {
+    private static Set<Integer> solution(int[] size) {
         Set<Integer> result = new HashSet<>();
-        boolean[][][] isVisited = new boolean[a + 1][b + 1][c + 1];
+        boolean[][][] isVisited = new boolean[size[0] + 1][size[1] + 1][size[2] + 1];
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, c});
+        q.add(new int[]{0, 0, size[2]});
 
-        int[] size = {a, b, c};
         while (!q.isEmpty()) {
             int[] bucket = q.poll();
+
             if (bucket[0] == 0) {
                 result.add(bucket[2]);
             }
-            for (int i = 0; i < 3; i++) {
-                int[] next = new int[3];
-                int t = Math.min(bucket[i], size[(i + 1) % 3] - bucket[(i + 1) % 3]);
-                next[i] = bucket[i] - t;
-                next[(i + 1) % 3] = bucket[(i + 1) % 3] + t;
-                next[(i + 2) % 3] = bucket[(i + 2) % 3];
-                if (!isVisited[next[0]][next[1]][next[2]]) {
-                    q.add(next);
-                    isVisited[next[0]][next[1]][next[2]] = true;
-                }
 
-                next = new int[3];
-                int r = Math.min(bucket[i], size[(i + 2) % 3] - bucket[(i + 2) % 3]);
-                next[i] = bucket[i] - r;
-                next[(i + 1) % 3] = bucket[(i + 1) % 3];
-                next[(i + 2) % 3] = bucket[(i + 2) % 3] + r;
-                if (!isVisited[next[0]][next[1]][next[2]]) {
-                    q.add(next);
-                    isVisited[next[0]][next[1]][next[2]] = true;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 1; j < 3; j++) {
+                    int[] next = new int[3];
+                    int min = Math.min(bucket[i], size[(i + 1) % 3] - bucket[(i + 1) % 3]);
+                    next[i] = bucket[i] - min;
+                    next[(i + 1) % 3] = bucket[(i + 1) % 3] + min;
+                    next[(i + 2) % 3] = bucket[(i + 2) % 3];
+                    if (!isVisited[next[0]][next[1]][next[2]]) {
+                        q.add(next);
+                        isVisited[next[0]][next[1]][next[2]] = true;
+                    }
                 }
             }
         }
@@ -51,7 +44,7 @@ public class Main {
             init[i] = Integer.parseInt(st.nextToken());
         }
 
-        Set<Integer> result = solution(init[0], init[1], init[2]);
+        Set<Integer> result = solution(init);
         ArrayList<Integer> s = new ArrayList<>(result);
         Collections.sort(s);
         for (int num : s) {
