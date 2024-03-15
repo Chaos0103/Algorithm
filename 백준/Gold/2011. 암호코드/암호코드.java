40b4f8430582113567b7fbb1ff5c63842e841d7e
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-03-15 14:59:18 +0900

    [Gold V] Title: 암호코드, Time: 124 ms, Memory: 14444 KB -BaekjoonHub

--- 백준/Gold/2011. 암호코드/암호코드.java
+++ 백준/Gold/2011. 암호코드/암호코드.java
@@ -1,43 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
     private static final int MOD = 1000000;
 
     public static void main(String[] args) throws IOException {
+        //입력부
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String pwd = br.readLine();
+
+        //시작이 0인 경우 해석 불가
         if (pwd.startsWith("0")) {
             System.out.println(0);
             return;
         }
 
-        if (pwd.contains("00")) {
-            System.out.println(0);
-            return;
-        }
-
+        //DP 테이블 초기화
         int[] dp = new int[pwd.length() + 1];
         dp[0] = 1;
         dp[1] = 1;
 
-        char prev = pwd.charAt(0);
         for (int i = 2; i <= pwd.length(); i++) {
-            if (Integer.parseInt(pwd.substring(i - 1, i)) == 0) {
-                if (prev > '2') {
-                    System.out.println(0);
-                    return;
-                }
+            //이전 숫자랑 현재 숫자로 두자리 수 만들기
+            int now = Integer.parseInt(pwd.substring(i - 2, i));
+            if (now == 10 || now == 20) {
                 dp[i] = dp[i - 2];
+            } else if (now % 10 == 0) {
+                dp[pwd.length()] = 0;
+                break;
             } else {
-                int now = Integer.parseInt(prev + pwd.substring(i - 1, i));
                 dp[i] = 10 < now && now < 27 ? (dp[i - 1] + dp[i - 2]) % MOD : dp[i - 1];
             }
-            prev = pwd.charAt(i - 1);
         }
-        System.out.println(dp[pwd.length()]);//35
+
+        System.out.println(dp[pwd.length()]);
     }
 }
