e5dd6fc526958ff174eba31f14d8af295164fb80
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-03-15 15:19:54 +0900

    [Gold V] Title: 택배 배송, Time: 508 ms, Memory: 40960 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.java
+++ 백준/Gold/5972. 택배 배송/택배 배송.java
@@ -1,100 +1,75 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
+import java.util.*;
 
-class Node implements Comparable<Node> {
-    private int index;
-    private int distance;
-
-    public Node (int index, int distance) {
-        this.index = index;
-        this.distance = distance;
-    }
-
-    public int getIndex() {
-        return this.index;
-    }
-
-    public int getDistance() {
-        return this.distance;
-    }
-
-    @Override
-    public int compareTo(Node o) {
-        if (this.distance < o.distance) {
-            return -1;
-        }
-        return 1;
-    }
-}
 public class Main {
 
-    private static int n, m;
-    private final static int INF = (int) 1e9;
-    private static ArrayList<ArrayList<Node>> graph = new ArrayList<>();
-    private static int start = 1;
-    private static int[] d = new int[50001];
+    private static final int INF = (int) 1e9;
+    private static final ArrayList<ArrayList<Node>> graph = new ArrayList<>();
+
+    private static class Node implements Comparable<Node> {
+        public int index;
+        public int distance;
+
+        public Node(int index, int distance) {
+            this.index = index;
+            this.distance = distance;
+        }
+
+        @Override
+        public int compareTo(Node o) {
+            return distance - o.distance;
+        }
+    }
+
+    private static void dijkstra(int[] d) {
+        PriorityQueue<Node> q = new PriorityQueue<>();
+        q.add(new Node(1, 0));
+
+        while (!q.isEmpty()) {
+            Node node = q.poll();
+            int index = node.index;
+            int distance = node.distance;
+
+            if (d[index] < distance) {
+                continue;
+            }
+
+            for (int i = 0; i < graph.get(index).size(); i++) {
+                int cost = distance + graph.get(index).get(i).distance;
+                if (d[graph.get(index).get(i).index] > cost) {
+                    d[graph.get(index).get(i).index] = cost;
+                    q.add(new Node(graph.get(index).get(i).index, cost));
+                }
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        final int n = Integer.parseInt(st.nextToken());
+        final int m = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-
-        for (int i=0; i<=n; i++) {
+        for (int i = 0; i <= n; i++) {
             graph.add(new ArrayList<>());
         }
 
-        for (int i=0; i<m; i++) {
+        for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
-
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-
-            graph.get(a).add(new Node(b,c));
-            graph.get(b).add(new Node(a,c));
+            graph.get(a).add(new Node(b, c));
+            graph.get(b).add(new Node(a, c));
         }
 
+        int[] d = new int[n + 1];
         Arrays.fill(d, INF);
 
-        dijkstra();
+        dijkstra(d);
 
         System.out.println(d[n]);
     }
-
-    private static void dijkstra() {
-
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-
-        pq.add(new Node(start,0));
-        d[start] = 0;
-
-        while (!pq.isEmpty()) {
-            Node node = pq.poll();
-            int dist = node.getDistance();
-            int now = node.getIndex();
-
-            if (d[now] < dist) {
-                continue;
-            }
-
-            for (int i=0; i<graph.get(now).size(); i++) {
-                int cost = d[now] + graph.get(now).get(i).getDistance();
-
-                if (cost < d[graph.get(now).get(i).getIndex()]) {
-                    d[graph.get(now).get(i).getIndex()] = cost;
-                    pq.add(new Node(graph.get(now).get(i).getIndex(), cost));
-                }
-            }
-        }
-
-    }//dijkstra
-
-
 }
