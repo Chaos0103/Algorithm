b0cea509c37ac1234b6ac88abff8a1248582099a
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-05-14 15:54:28 +0900

    [Silver IV] Title: 괄호, Time: 132 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.java
+++ 백준/Silver/9012. 괄호/괄호.java
@@ -1,43 +1,34 @@
-import java.io.*;
-import java.util.Stack;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-
-    private static boolean isVPS(String target) {
-        Stack<String> stack = new Stack<>();
-        for (char c : target.toCharArray()) {
-            if (c == '(') {
-                stack.push(String.valueOf(c));
-                continue;
-            }
-
-            if (stack.empty()) {
-                return false;
-            }
-
-            stack.pop();
-        }
-
-        return stack.empty();
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder builder = new StringBuilder();
-
 
         int n = Integer.parseInt(br.readLine());
+
+
         for (int i = 0; i < n; i++) {
-            String str = br.readLine();
+            char[] targets = br.readLine().toCharArray();
 
-            String result = isVPS(str) ? "YES" : "NO";
+            int count = 0;
+            for (char target : targets) {
+                if (target == '(') {
+                    count++;
+                } else {
+                    count--;
+                    if (count < 0) {
+                        break;
+                    }
+                }
+            }
 
-            builder.append(result).append("\n");
+            if (count != 0) {
+                System.out.println("NO");
+            } else {
+                System.out.println("YES");
+            }
         }
-
-        bw.write(builder.toString());
-        bw.flush();
-        bw.close();
     }
 }
