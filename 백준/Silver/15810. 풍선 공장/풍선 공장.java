fafdd0b2ad954f44b8421ce4958009515bf7bbf5
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-05-28 11:43:16 +0900

    [Silver II] Title: 풍선 공장, Time: 840 ms, Memory: 92048 KB -BaekjoonHub

---
+++ 백준/Silver/15810. 풍선 공장/풍선 공장.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long left = 0;
+        long right = 1_000_000_000_000L;
+        long result = 0;
+        while (left <= right) {
+            long mid = (left + right) / 2;
+            long cnt = 0;
+            for (int i = 0; i < n; i++) {
+                cnt += mid / arr[i];
+            }
+            if (cnt >= m) {
+                result = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
