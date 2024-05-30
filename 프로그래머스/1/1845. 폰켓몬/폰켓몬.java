04fb5edffd3489321abcd503c35117955ae373a5
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-05-30 11:20:00 +0900

    [level 1] Title: 폰켓몬, Time: 2.88 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,12 +1,16 @@
 import java.util.*;
 
 class Solution {
+    //1. set에 nums를 삽입
+    //2. set의 크기와 nums.length/2의 크기를 비교
+    //2-1. set의 크기가 크거나 같다면 nums.length 반환
+    //2-2. set의 크기가 작다면 set의 크기 반환
     public int solution(int[] nums) {
-        Set<Integer> types = new HashSet<>();
-        for (int type : nums) {
-            types.add(type);
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            set.add(num);
         }
         
-        return nums.length / 2 <= types.size() ? nums.length / 2 : types.size();
+        return set.size() < nums.length / 2 ? set.size() : nums.length / 2;
     }
 }
