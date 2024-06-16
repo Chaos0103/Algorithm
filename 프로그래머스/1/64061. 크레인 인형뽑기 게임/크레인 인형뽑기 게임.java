669213defa3c037d48fb7446c386562ff2952f59
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-06-16 14:33:11 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 0.70 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,41 +1,25 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
 import java.util.Stack;
 
 class Solution {
-
-    private static final List<Stack<Integer>> boards = new ArrayList<>();
-    private static final Stack<Integer> basket = new Stack<>();
-
     public int solution(int[][] board, int[] moves) {
-        for (int i = 0; i < board.length; i++) {
-            boards.add(new Stack<>());
-        }
-
-        for (int i = board.length - 1; i >= 0; i--) {
-            for (int j = 0; j < board.length; j++) {
-                if (board[i][j] > 0) {
-                    boards.get(j).add(board[i][j]);
-                }
-            }
-        }
-
         int answer = 0;
 
+        Stack<Integer> basket = new Stack<>();
         for (int move : moves) {
-            if (boards.get(move - 1).isEmpty()) {
-                continue;
-            }
+            for (int i = 0; i < board.length; i++) {
+                if (board[i][move - 1] > 0) {
+                    int dall = board[i][move - 1];
+                    board[i][move - 1] = 0;
+                    if (!basket.isEmpty() && basket.peek() == dall) {
+                        answer += 2;
+                        basket.pop();
+                    } else {
+                        basket.add(dall);
+                    }
 
-            if (!basket.isEmpty() && Objects.equals(boards.get(move - 1).peek(), basket.peek())) {
-                answer += 2;
-                basket.pop();
-                boards.get(move - 1).pop();
-                continue;
+                    break;
+                }
             }
-
-            basket.add(boards.get(move - 1).pop());
         }
 
         return answer;
