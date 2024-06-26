b62c20c2d9a6f7989787d7c595cb5cbe69cb04d2
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-06-26 14:46:03 +0900

    [level 1] Title: 폰켓몬, Time: 3.35 ms, Memory: 77.8 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 class Solution {
-    //1. set에 nums를 삽입
-    //2. set의 크기와 nums.length/2의 크기를 비교
-    //2-1. set의 크기가 크거나 같다면 nums.length 반환
-    //2-2. set의 크기가 작다면 set의 크기 반환
+    //총 N마리중 N/2마리 가져갈 수 있음
+    //같은 종류의 폰켓몬은 같은 번호
+    //가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아 갯수를 반환
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-        
-        return Math.min(set.size(), nums.length / 2);
+    
+        int half = nums.length / 2;
+        return set.size() < half ? set.size() : half;
     }
 }
