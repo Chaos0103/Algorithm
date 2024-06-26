7a32b1f5640ce9ed5929e52ca5fdd7f8520eea9c
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-06-26 16:21:06 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -3,15 +3,14 @@ import java.util.*;
 class Solution {
     public int solution(String[][] clothes) {
         Map<String, Integer> map = new HashMap<>();
-        for (String[] clothesInfo : clothes) {
-            int count = map.getOrDefault(clothesInfo[1], 0);
-            map.put(clothesInfo[1], count + 1);
+        for (String[] cloth : clothes) {
+            int count = map.getOrDefault(cloth[1], 0);
+            map.put(cloth[1], count + 1);
         }
         
-        int answer = 1;
-        for (String name : map.keySet()) {
-            int count = map.get(name);
-            answer *= (count + 1);
+        int answer = 1; 
+        for (String type : map.keySet()) {
+            answer *= map.get(type) + 1;
         }
         
         return answer - 1;
