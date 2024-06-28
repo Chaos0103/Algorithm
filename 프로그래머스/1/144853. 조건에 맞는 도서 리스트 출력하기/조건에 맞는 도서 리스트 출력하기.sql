a61f97e0ed16257cb883f6f89acf627cfc417e0d
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-06-28 21:49:39 +0900

    [level 1] Title: 조건에 맞는 도서 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
+++ 프로그래머스/1/144853. 조건에 맞는 도서 리스트 출력하기/조건에 맞는 도서 리스트 출력하기.sql
@@ -1,4 +1,4 @@
-select BOOK_ID, DATE_FORMAT(PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
-from BOOK
-where CATEGORY='인문' and year(PUBLISHED_DATE)=2021
-order by PUBLISHED_DATE;
+select book_id, date_format(published_date, '%Y-%m-%d') as 'published_date'
+from book
+where category='인문' and year(published_date)=2021
+order by published_date
