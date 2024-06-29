2cf313c51654b9b6e1946a6f05cd956e879dd1c7
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-06-29 13:28:39 +0900

    [level 2] Title: 소수 찾기, Time: 2.69 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
 
 ### 성능 요약
 
-메모리: 85.6 MB, 시간: 16.95 ms
+메모리: 80.3 MB, 시간: 2.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 0일 16:22:19
+2024년 06월 29일 13:28:36
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,52 +1,45 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 class Solution {
-
-    private Set<Integer> dfs(char[] arr, int[] visited, String s) {
-        Set<Integer> set = new HashSet<>();
-        if (!s.isEmpty()) {
-            set.add(Integer.parseInt(s));
+    
+    private static final Set<Integer> set = new HashSet<>();
+    
+    private void comb(String num, boolean[] isUsed, String ans, int depth, int length) {
+        if (depth == length) {
+            int temp = Integer.parseInt(ans);
+            if (isPrime(temp)) {
+                set.add(temp);    
+            }
+            return;
         }
-
-        if (s.length() == arr.length) {
-            return set;
-        }
-
-        for (int i = 0; i < 10; i++) {
-            if (visited[i] > 0) {
-                visited[i]--;
-                set.addAll(dfs(arr, visited, s + i));
-                visited[i]++;
+        
+        for (int i = 0; i < num.length(); i++) {
+            if (!isUsed[i]) {
+                isUsed[i] = true;
+                comb(num, isUsed, ans + num.substring(i, i + 1), depth + 1, length);
+                isUsed[i] = false;
             }
         }
-
-        return set;
     }
-
-    private boolean isPrime(int number) {
-        if (number < 2) {
+    
+    private boolean isPrime(int num) {
+        if (num < 2) {
             return false;
         }
-
-        for (int i = 2; i < number; i++) {
-            if (number % i == 0) {
+        
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
                 return false;
             }
         }
         return true;
     }
-
+    
     public int solution(String numbers) {
-        char[] arr = numbers.toCharArray();
-        int[] counts = new int[10];
-        for (char c : arr) {
-            counts[c - '0']++;
+        for (int i = 1; i <= numbers.length(); i++) {
+            comb(numbers, new boolean[numbers.length()], "", 0, i);
         }
-        Set<Integer> result = dfs(arr, counts, "");
-
-        return (int) result.stream()
-            .filter(this::isPrime)
-            .count();
+        System.out.println(set);
+        return set.size();
     }
 }
