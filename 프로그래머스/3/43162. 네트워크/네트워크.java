caf13e112279055f9722f9b1dde7e7f3b83bc80a
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-07-06 16:09:54 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,47 +1,24 @@
-import java.util.*;
-
 class Solution {
     
-    private static final List<List<Integer>> graph = new ArrayList<>();
-    
-    private void bfs(int start, boolean[] isVisited) {
-        Queue<Integer> q = new LinkedList<>();
-        q.add(start);
-        
-        while (!q.isEmpty()) {
-            int num = q.poll();
-            isVisited[num] = true;
-            for (int i = 0; i < graph.get(num).size(); i++) {
-                int next = graph.get(num).get(i);
-                if (isVisited[next]) {
-                    continue;
-                }
-                q.add(next);
+    private void dfs(int[][] computers, boolean[] isVisited, int num) {
+        isVisited[num] = true;
+        for (int i = 0; i < isVisited.length; i++) {
+            if (computers[num][i] == 1 && !isVisited[i]) {
+                dfs(computers, isVisited, i);
             }
         }
     }
     
     public int solution(int n, int[][] computers) {
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    graph.get(i).add(j);
-                }
-            }
-        }
-        
-        boolean[] isVisited = new boolean[n];
         int result = 0;
+        boolean[] isVisited = new boolean[n];
         for (int i = 0; i < n; i++) {
             if (!isVisited[i]) {
-                bfs(i, isVisited);
+                dfs(computers, isVisited, i);
                 result++;
             }
         }
+        
         return result;
     }
 }
