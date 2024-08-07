c8db17f99cc2dc485b081f9ad7921ed69b670c79
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-08-07 15:57:51 +0900

    [Gold V] Title: 토마토, Time: 564 ms, Memory: 103644 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,43 +1,22 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int[][] d = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+    private static final int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private static final Queue<Point> q = new ArrayDeque<>();
 
-    private static class Tomato {
-        public int x;
+    private static class Point {
         public int y;
+        public int x;
 
-        public Tomato(int x, int y) {
-            this.x = x;
+        public Point(int y, int x) {
             this.y = y;
-        }
-    }
-
-    private static void bfs(int[][] map, int n, int m, List<Tomato> tomatoes) {
-        Queue<Tomato> q = new LinkedList<>(tomatoes);
-
-        while (!q.isEmpty()) {
-            Tomato tomato = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int nx = tomato.x + d[i][0];
-                int ny = tomato.y + d[i][1];
-
-                if (!(0 <= nx && nx < m && 0 <= ny && ny < n)) {
-                    continue;
-                }
-
-                if (map[ny][nx] != 0) {
-                    continue;
-                }
-
-                map[ny][nx] = map[tomato.y][tomato.x] + 1;
-                q.add(new Tomato(nx, ny));
-            }
+            this.x = x;
         }
     }
 
@@ -49,18 +28,33 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
 
         int[][] map = new int[n][m];
-        List<Tomato> tomatoes = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if (map[i][j] == 1) {
-                    tomatoes.add(new Tomato(j, i));
+                    q.offer(new Point(i, j));
                 }
             }
         }
 
-        bfs(map, n, m, tomatoes);
+        while (!q.isEmpty()) {
+            Point p = q.poll();
+            for (int i = 0; i < 4; i++) {
+                int ny = p.y + d[i][0];
+                int nx = p.x + d[i][1];
+                if (!(0 <= ny && ny < n && 0 <= nx && nx < m)) {
+                    continue;
+                }
+
+                if (map[ny][nx] != 0) {
+                    continue;
+                }
+
+                map[ny][nx] = map[p.y][p.x] + 1;
+                q.offer(new Point(ny, nx));
+            }
+        }
 
         int result = 0;
         for (int i = 0; i < n; i++) {
