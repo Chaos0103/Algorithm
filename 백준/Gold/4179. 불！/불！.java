74ef11c96d2e3895098c149efaa7fbb5578802df
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-08-16 23:04:04 +0900

    [Gold III] Title: 불!, Time: 424 ms, Memory: 46932 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,10 +1,14 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    private static final Queue<Point> jihun = new ArrayDeque<>();
+    private static final Queue<Point> fire = new ArrayDeque<>();
     private static final int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     private static class Point {
@@ -15,106 +19,89 @@ public class Main {
             this.y = y;
             this.x = x;
         }
-    }
 
-    private static int[][] fire(char[][] map, ArrayList<Point> fires) {
-        int[][] result = new int[map.length][map[0].length];
-        for (Point fire : fires) {
-            result[fire.y][fire.x] = 1;
+        @Override
+        public String toString() {
+            return "Point{" +
+                "y=" + y +
+                ", x=" + x +
+                '}';
         }
-
-        Queue<Point> q = new LinkedList<>(fires);
-
-        while (!q.isEmpty()) {
-            Point point = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int ny = point.y + d[i][0];
-                int nx = point.x + d[i][1];
-
-                if (!(0 <= ny && ny < map.length && 0 <= nx && nx < map[0].length)) {
-                    continue;
-                }
-
-                if (map[ny][nx] == '#') {
-                    continue;
-                }
-
-                if (result[ny][nx] > 0) {
-                    continue;
-                }
-
-                result[ny][nx] = result[point.y][point.x] + 1;
-                q.add(new Point(ny, nx));
-            }
-        }
-
-        return result;
-    }
-
-    private static int jihun(char[][] map, int[][] fire, Point jihun) {
-        int[][] result = new int[map.length][map[0].length];
-        result[jihun.y][jihun.x] = 1;
-
-        Queue<Point> q = new LinkedList<>();
-        q.add(jihun);
-
-        while (!q.isEmpty()) {
-            Point point = q.poll();
-
-            for (int i = 0; i < 4; i++) {
-                int ny = point.y + d[i][0];
-                int nx = point.x + d[i][1];
-
-                if (!(0 <= ny && ny < map.length && 0 <= nx && nx < map[0].length)) {
-                    return result[point.y][point.x];
-                }
-
-                if (map[ny][nx] == '#') {
-                    continue;
-                }
-
-                if (result[ny][nx] > 0) {
-                    continue;
-                }
-
-                result[ny][nx] = result[point.y][point.x] + 1;
-                if (fire[ny][nx] > 0 && result[ny][nx] >= fire[ny][nx]) {
-                    continue;
-                }
-
-                q.add(new Point(ny, nx));
-            }
-        }
-
-        return -1;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        Point jihun = null;
-        ArrayList<Point> fires = new ArrayList<>();
-
+        boolean[][] isVisited = new boolean[n][m];
         char[][] map = new char[n][m];
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 map[i][j] = line.charAt(j);
                 if (map[i][j] == 'J') {
-                    jihun = new Point(i, j);
+                    jihun.offer(new Point(i, j));
+                    isVisited[i][j] = true;
+                    map[i][j] = '.';
                 } else if (map[i][j] == 'F') {
-                    fires.add(new Point(i, j));
+                    fire.offer(new Point(i, j));
                 }
             }
         }
 
-        int[][] fire = fire(map, fires);
-        int result = jihun(map, fire, jihun);
-        System.out.println(result > 0 ? result : "IMPOSSIBLE");
+
+        int time = 0;
+        while (!jihun.isEmpty()) {
+            time++;
+            int size = fire.size();
+            while (size-- > 0) {
+                Point fp = fire.poll();
+                for (int i = 0; i < 4; i++) {
+                    int ny = fp.y + d[i][0];
+                    int nx = fp.x + d[i][1];
+
+                    if (!(0 <= ny && ny < n && 0 <= nx && nx < m)) {
+                        continue;
+                    }
+
+                    if (map[ny][nx] != '.') {
+                        continue;
+                    }
+
+                    map[ny][nx] = 'F';
+                    fire.offer(new Point(ny, nx));
+                }
+            }
+
+            int jSize = jihun.size();
+            while (jSize-- > 0) {
+                Point jp = jihun.poll();
+                for (int i = 0; i < 4; i++) {
+                    int ny = jp.y + d[i][0];
+                    int nx = jp.x + d[i][1];
+
+                    if (!(0 <= ny && ny < n && 0 <= nx && nx < m)) {
+                        System.out.println(time);
+                        return;
+                    }
+
+                    if (map[ny][nx] != '.') {
+                        continue;
+                    }
+
+                    if (isVisited[ny][nx]) {
+                        continue;
+                    }
+
+                    isVisited[ny][nx] = true;
+                    jihun.offer(new Point(ny, nx));
+                }
+            }
+        }
+
+        System.out.println("IMPOSSIBLE");
     }
 }
