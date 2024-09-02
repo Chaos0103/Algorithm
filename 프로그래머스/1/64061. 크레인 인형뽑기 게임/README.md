1554f974cedc421b2e04dc1c0b44e42a53285285
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-09-02 16:32:11 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.61 ms, Memory: 81.3 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 75.6 MB, 시간: 0.70 ms
+메모리: 81.3 MB, 시간: 1.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 16일 14:33:10
+2024년 09월 02일 16:32:08
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.java
@@ -1,27 +1,45 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
+    private int run(Deque<Integer>[] board, int[] moves) {
+        int result = 0;
+        Deque<Integer> basket = new ArrayDeque<>();
+        
+        for (int i = 0; i < moves.length; i++) {
+            int index = moves[i] - 1;
+            if (board[index].isEmpty()) {
+                continue;
+            }
+        
+            int no = board[index].pop();
+            if (basket.isEmpty() || basket.peek() != no) {
+                basket.push(no);
+                continue;
+            }
+            
+            basket.pop();
+            result += 2;
+        }
+        
+        return result;
+    }
+    
     public int solution(int[][] board, int[] moves) {
-        int answer = 0;
-
-        Stack<Integer> basket = new Stack<>();
-        for (int move : moves) {
-            for (int i = 0; i < board.length; i++) {
-                if (board[i][move - 1] > 0) {
-                    int dall = board[i][move - 1];
-                    board[i][move - 1] = 0;
-                    if (!basket.isEmpty() && basket.peek() == dall) {
-                        answer += 2;
-                        basket.pop();
-                    } else {
-                        basket.add(dall);
-                    }
-
-                    break;
+        int n = board[0].length;
+        
+        Deque<Integer>[] s = new Deque[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = new ArrayDeque<>();
+        }
+        
+        for (int i = board.length - 1; i >= 0; i--) {
+            for(int j = 0; j < board[i].length; j++) {
+                if (board[i][j] > 0) {
+                    s[j].push(board[i][j]);    
                 }
             }
         }
-
-        return answer;
+        
+        return run(s, moves);
     }
 }
