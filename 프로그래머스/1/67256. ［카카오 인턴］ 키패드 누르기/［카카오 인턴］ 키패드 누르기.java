050dc51f91af9d103c76001081de2251661e95ba
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-09-02 22:46:54 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 8.53 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,64 +1,72 @@
-import java.util.*;
+import java.util.Set;
 
 class Solution {
-    
+
     private static final int[][] NUMBERS = {
         {3, 1},
         {0, 0}, {0, 1}, {0, 2},
         {1, 0}, {1, 1}, {1, 2},
         {2, 0}, {2, 1}, {2, 2},
     };
-    private static Point left;
-    private static Point right;
-    
+    private Point left;
+    private Point right;
+    private String mainHand;
+
     private static class Point {
         public int y;
         public int x;
-        
+
         public Point(int y, int x) {
             this.y = y;
             this.x = x;
         }
     }
-    
+
     private int calculateDistance(int[] arr, Point p) {
         return Math.abs(arr[0] - p.y) + Math.abs(arr[1] - p.x);
     }
-    
+
+    private String getFinger(int num) {
+        if (Set.of(1, 4, 7).contains(num)) {
+            return "L";
+        }
+
+        if (Set.of(3, 6, 9).contains(num)) {
+            return "R";
+        }
+
+        int leftDistance = calculateDistance(NUMBERS[num], left);
+        int rightDistance = calculateDistance(NUMBERS[num], right);
+
+        if (leftDistance < rightDistance) {
+            return "L";
+        }
+
+        if (leftDistance > rightDistance) {
+            return "R";
+        }
+
+        return mainHand;
+    }
+
     public String solution(int[] numbers, String hand) {
+        mainHand = "left".equals(hand) ? "L" : "R";
         left = new Point(3, 0);
         right = new Point(3, 2);
-        
+
         StringBuilder sb = new StringBuilder();
         for (int number : numbers) {
+            String finger = getFinger(number);
+
             int[] p = NUMBERS[number];
-            if (List.of(1, 4, 7).contains(number)) {
+            if ("L".equals(finger)) {
                 left = new Point(p[0], p[1]);
-                sb.append("L");
-            } else if (List.of(3, 6, 9).contains(number)) {
-                right = new Point(p[0], p[1]);
-                sb.append("R");
             } else {
-                int leftDistance = calculateDistance(p, left);
-                int rightDistance = calculateDistance(p, right);
-                if (leftDistance < rightDistance) {
-                    left = new Point(p[0], p[1]);
-                    sb.append("L");
-                } else if (leftDistance > rightDistance) {
-                    right = new Point(p[0], p[1]);
-                    sb.append("R");
-                } else {
-                    if ("left".equals(hand)) {
-                        left = new Point(p[0], p[1]);
-                        sb.append("L");
-                    } else {
-                        right = new Point(p[0], p[1]);
-                        sb.append("R");
-                    }
-                }
+                right = new Point(p[0], p[1]);
             }
+            sb.append(finger);
         }
-        
+
         return sb.toString();
     }
 }
