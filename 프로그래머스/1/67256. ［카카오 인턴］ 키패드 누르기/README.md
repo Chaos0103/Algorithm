5794783da00737c6e32663f352a588aa4cc90d01
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-09-02 21:06:43 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 2.22 ms, Memory: 78.6 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 68.3 MB, 시간: 0.44 ms
+메모리: 78.6 MB, 시간: 2.22 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 26일 15:27:37
+2024년 09월 02일 21:06:40
 
 ### 문제 설명
 
--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,71 +1,64 @@
+import java.util.*;
+
 class Solution {
+    
+    private static final int[][] NUMBERS = {
+        {3, 1},
+        {0, 0}, {0, 1}, {0, 2},
+        {1, 0}, {1, 1}, {1, 2},
+        {2, 0}, {2, 1}, {2, 2},
+    };
+    private static Point left;
+    private static Point right;
+    
     private static class Point {
-        public int x;
         public int y;
-
-        public Point(int x, int y) {
-            this.x = x;
+        public int x;
+        
+        public Point(int y, int x) {
             this.y = y;
+            this.x = x;
         }
     }
-
-    private static final Point[] numberPoints = new Point[10];
-    private Point left = new Point(0, 3);
-    private Point right = new Point(2, 3);
-
-    private void initNumberPoint() {
-        numberPoints[0] = new Point(1, 3);
-        int index = 1;
-        for (int y = 0; y < 3; y++) {
-            for (int x = 0; x < 3; x++) {
-                numberPoints[index++] = new Point(x, y);
-            }
-        }
+    
+    private int calculateDistance(int[] arr, Point p) {
+        return Math.abs(arr[0] - p.y) + Math.abs(arr[1] - p.x);
     }
-
-    private String move(Point point, String hand) {
-        int leftDistance = calcDistance(point, left);
-        int rightDistance = calcDistance(point, right);
-        String result;
-        if (leftDistance > rightDistance) {
-            right = point;
-            result = "R";
-        } else if (leftDistance < rightDistance) {
-            left = point;
-            result = "L";
-        } else {
-            if (hand.equals("right")) {
-                right = point;
-                result = "R";
-            } else {
-                left = point;
-                result = "L";
-            }
-        }
-        return result;
-    }
-
-    private int calcDistance(Point a, Point b) {
-        return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
-    }
-
+    
     public String solution(int[] numbers, String hand) {
-        initNumberPoint();
-
+        left = new Point(3, 0);
+        right = new Point(3, 2);
+        
         StringBuilder sb = new StringBuilder();
         for (int number : numbers) {
-            Point point = numberPoints[number];
-            if (number % 3 == 2 || number == 0) {
-                String move = move(point, hand);
-                sb.append(move);
-            } else if (number % 3 == 1) {
-                left = point;
+            int[] p = NUMBERS[number];
+            if (List.of(1, 4, 7).contains(number)) {
+                left = new Point(p[0], p[1]);
                 sb.append("L");
-            } else {
-                right = point;
+            } else if (List.of(3, 6, 9).contains(number)) {
+                right = new Point(p[0], p[1]);
                 sb.append("R");
+            } else {
+                int leftDistance = calculateDistance(p, left);
+                int rightDistance = calculateDistance(p, right);
+                if (leftDistance < rightDistance) {
+                    left = new Point(p[0], p[1]);
+                    sb.append("L");
+                } else if (leftDistance > rightDistance) {
+                    right = new Point(p[0], p[1]);
+                    sb.append("R");
+                } else {
+                    if ("left".equals(hand)) {
+                        left = new Point(p[0], p[1]);
+                        sb.append("L");
+                    } else {
+                        right = new Point(p[0], p[1]);
+                        sb.append("R");
+                    }
+                }
             }
         }
+        
         return sb.toString();
     }
 }
