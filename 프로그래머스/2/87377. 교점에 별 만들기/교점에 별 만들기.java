42900d2ac205641cba45f0a0fab55a3cacde52ee
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-09-25 11:47:56 +0900

    [level 2] Title: 교점에 별 만들기, Time: 112.91 ms, Memory: 229 MB -BaekjoonHub

--- 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
+++ 프로그래머스/2/87377. 교점에 별 만들기/교점에 별 만들기.java
@@ -1,81 +1,61 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-    //좌표를 저장할 클래스
-    private static class Point {
-        public long x;
-        public long y;
-
-        public Point(long x, long y) {
-            this.x = x;
-            this.y = y;
-        }
+    private boolean hasUniquePoint(long a, long b, long c, long d) {
+        long calc = a * d - b * c;
+        return calc != 0;
     }
-
-    //1. 교점을 구하기
-    private Point calculateCrossPoint(int[] line1, int[] line2) {
-        long a = line1[0];
-        long b = line1[1];
-        long e = line1[2];
-        long c = line2[0];
-        long d = line2[1];
-        long f = line2[2];
-        double x = (double) (b * f - e * d) / (a * d - b * c);
-        double y = (double) (e * c - a * f) / (a * d - b * c);
-        //2. 교점이 정수인 경우 저장하기
-        if (x % 1 != 0 || y % 1 != 0) {
+    
+    private int[] calculateCrossPoint(long a, long b, long e, long c, long d, long f) {
+        long x = (b * f - e * d) / (a * d - b * c);
+        long y = (e * c - a * f) / (a * d - b * c);
+        if ((b * f - e * d) % (a * d - b * c) != 0 || (e * c - a * f) % (a * d - b * c) != 0) {
             return null;
         }
-
-        return new Point((long) x, (long) y);
+        return new int[]{(int) x, (int) y};
     }
-
+    
     public String[] solution(int[][] line) {
-        long maxX = Long.MIN_VALUE;
-        long minX = Long.MAX_VALUE;
-
-        long maxY = Long.MIN_VALUE;
-        long minY = Long.MAX_VALUE;
-
-        List<Point> points = new ArrayList<>();
+        List<int[]> points = new ArrayList<>();
         for (int i = 0; i < line.length; i++) {
             for (int j = i + 1; j < line.length; j++) {
-                Point point = calculateCrossPoint(line[i], line[j]);
-                if (point != null) {
-                    points.add(point);
-                    //3. x, y축 최댓값, 최솟값 구하기
-                    maxX = Math.max(maxX, point.x);
-                    minX = Math.min(minX, point.x);
-
-                    maxY = Math.max(maxY, point.y);
-                    minY = Math.min(minY, point.y);
+                if (hasUniquePoint(line[i][0], line[i][1], line[j][0], line[j][1])) {
+                    int[] point = calculateCrossPoint(line[i][0], line[i][1], line[i][2], line[j][0], line[j][1], line[j][2]);
+                    if (point != null) {
+                        points.add(point);    
+                    }
                 }
             }
         }
-
-        //4. 별 찍을 배열 생성
-        boolean[][] star = new boolean[(int) (maxY - minY + 1)][(int) (maxX - minX + 1)];
-
-        //5. 별 찍기
-        for (Point point : points) {
-            star[(int) (point.y - minY)][(int) (point.x - minX)] = true;
+        
+        int[] max = new int[]{Integer.MIN_VALUE, Integer.MIN_VALUE};
+        int[] min = new int[]{Integer.MAX_VALUE, Integer.MAX_VALUE};
+        for (int[] point : points) {
+            max[0] = Math.max(max[0], point[0]);
+            max[1] = Math.max(max[1], point[1]);
+            min[0] = Math.min(min[0], point[0]);
+            min[1] = Math.min(min[1], point[1]);
         }
-
-        //6. String 배열로 변환
-        String[] answer = new String[star.length];
-        for (int i = 0; i < star.length; i++) {
-            StringBuilder sb = new StringBuilder();
-            for (int j = 0; j < star[i].length; j++) {
-                if (star[i][j]) {
-                    sb.append("*");
-                } else {
-                    sb.append(".");
+        
+        char[][] board = new char[max[1] - min[1] + 1][max[0] - min[0] + 1];
+        for (int[] point : points) {
+        
+            board[point[1] - min[1]][point[0] - min[0]] = '*';
+        }
+        
+        String[] answer = new String[board.length];
+        for (int i = 0; i < board.length; i++) {
+            String temp = "";
+            for (int j = 0; j < board[i].length; j++) {
+                char ch = board[board.length - 1 - i][j];
+                if (ch != '*') {
+                    ch = '.';
                 }
+                temp += ch;
             }
-            answer[answer.length - i - 1] = sb.toString();
+            answer[i] = temp;
         }
-
+        
         return answer;
     }
 }
