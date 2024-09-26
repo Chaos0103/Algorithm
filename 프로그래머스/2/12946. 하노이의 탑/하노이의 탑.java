393e9032b5d0aad8c41a83bbcd0b64ef121f6853
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-09-26 11:34:35 +0900

    [level 2] Title: 하노이의 탑, Time: 5.31 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,29 +1,28 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-
-    private static List<int[]> result = new ArrayList<>();
-
-    public int[][] solution(int n) {
-        hanoi(n, 1, 3);
-
-        int[][] answer = new int[result.size()][2];
-        for (int i = 0; i < result.size(); i++) {
-            answer[i] = result.get(i);
-        }
-        return answer;
-    }
-
+    
+    private static final List<int[]> result = new ArrayList<>();
+    
     private void hanoi(int n, int from, int to) {
         if (n == 1) {
             result.add(new int[]{from, to});
             return;
         }
-
+        
         int mid = 6 - from - to;
         hanoi(n - 1, from, mid);
         hanoi(1, from, to);
         hanoi(n - 1, mid, to);
+    }
+    
+    public int[][] solution(int n) {
+        hanoi(n, 1, 3);
+        
+        int[][] answer = new int[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            answer[i] = result.get(i);
+        }
+        return answer;
     }
 }
