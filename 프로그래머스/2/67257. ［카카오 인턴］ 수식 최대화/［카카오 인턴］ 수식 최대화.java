59f54aead07f384ee11e1b99787c08ca2ffd88f5
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-10-24 21:54:01 +0900

    [level 2] Title: [카카오 인턴] 수식 최대화, Time: 1.91 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
+++ 프로그래머스/2/67257. ［카카오 인턴］ 수식 최대화/［카카오 인턴］ 수식 최대화.java
@@ -3,35 +3,38 @@ import java.util.List;
 import java.util.StringTokenizer;
 
 class Solution {
-
-    private static final String[][] OPERATIONS = {
-        "+-*".split(""),
-        "+*-".split(""),
+    private static final String[][] operations = {
+        "*-+".split(""),
+        "*+-".split(""),
         "-*+".split(""),
         "-+*".split(""),
-        "*-+".split(""),
-        "*+-".split("")
+        "+*-".split(""),
+        "+-*".split("")
     };
 
+    private List<String> generateToken(String expression) {
+        StringTokenizer st = new StringTokenizer(expression, "*-+", true);
+        List<String> results = new ArrayList<>();
+        while (st.hasMoreTokens()) {
+            results.add(st.nextToken());
+        }
+        return results;
+    }
+
     private long calculate(List<String> tokens, int index) {
-        String[] operations = OPERATIONS[index];
-        for (String operation : operations) {
+        String[] operation = operations[index];
+        for (String op : operation) {
             for (int i = 0; i < tokens.size(); i++) {
-                String token = tokens.get(i);
-                if (token.equals(operation)) {
-                    long left = Long.parseLong(tokens.get(i - 1));
-                    long right = Long.parseLong(tokens.get(i + 1));
-                    long result = 0;
-                    switch (operation) {
-                        case "+":
-                            result = left + right;
-                            break;
-                        case "-":
-                            result = left - right;
-                            break;
-                        case "*":
-                            result = left * right;
-                            break;
+                if (tokens.get(i).equals(op)) {
+                    Long a = Long.parseLong(tokens.get(i - 1));
+                    Long b = Long.parseLong(tokens.get(i + 1));
+                    long result = 0L;
+                    if ("+".equals(op)) {
+                        result = a + b;
+                    } else if ("-".equals(op)) {
+                        result = a - b;
+                    } else if ("*".equals(op)) {
+                        result = a * b;
                     }
                     tokens.remove(i + 1);
                     tokens.remove(i);
@@ -46,16 +49,12 @@ class Solution {
     }
 
     public long solution(String expression) {
-        List<String> tokens = new ArrayList<>();
-        StringTokenizer token = new StringTokenizer(expression, "+-*", true);
-        while (token.hasMoreTokens()) {
-            tokens.add(token.nextToken());
-        }
+        List<String> tokens = generateToken(expression);
 
         long answer = 0;
-        for (int i = 0; i < 6; i++) {
-            long result = calculate(new ArrayList<>(tokens), i);
-            answer = Math.max(answer, Math.abs(result));
+        for (int i = 0; i < operations.length; i++) {
+            long calculate = calculate(new ArrayList<>(tokens), i);
+            answer = Math.max(answer, Math.abs(calculate));
         }
 
         return answer;
