6fc843c3817eaa50792847e67e1aab7cb9b8f074
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-10-24 20:31:28 +0900

    [level 1] Title: 모의고사, Time: 22.22 ms, Memory: 86.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,38 +1,34 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 class Solution {
-
-    private static final int[][] METHODS = {
+    private static final int[][] WAYS = {
         {1, 2, 3, 4, 5},
         {2, 1, 2, 3, 2, 4, 2, 5},
         {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
     };
-
+    
     public int[] solution(int[] answers) {
         int[] counts = new int[3];
+        
         int max = 0;
-
         for (int i = 0; i < 3; i++) {
-            int index = 0;
-            int size = METHODS[i].length;
-            for (int answer : answers) {
-                if (answer == METHODS[i][index++ % size]) {
+            int[] way = WAYS[i];
+            for (int j = 0; j < answers.length; j++) {
+                if (answers[j] == way[j % way.length]) {
                     counts[i]++;
                 }
             }
             max = Math.max(max, counts[i]);
         }
-
-        List<Integer> peoples = new ArrayList<>();
+        
+        List<Integer> results = new ArrayList<>();        
         for (int i = 0; i < 3; i++) {
-            if (counts[i] == max) {
-                peoples.add(i + 1);
+            if (max == counts[i]) {
+                results.add(i + 1);
             }
         }
-
-        return peoples.stream()
-            .mapToInt(people -> people)
+        return results.stream()
+            .mapToInt(Integer::valueOf)
             .toArray();
     }
 }
