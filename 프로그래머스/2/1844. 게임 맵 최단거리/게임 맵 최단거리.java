806631e04fd3f7a7d0ea5685489ef2362b8455d5
Dong82 <85945540+Chaos0103@users.noreply.github.com>
2024-10-28 16:12:23 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.42 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,56 +2,38 @@ import java.util.*;
 
 class Solution {
     
-    private static final int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    private int[][] d = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
     
-    private static class Point {
-        public int y;
-        public int x;
-        public int count;
+    public int solution(int[][] maps) {
+        boolean[][] isVisited = new boolean[maps.length][maps[0].length];
         
-        public Point(int y, int x, int count) {
-            this.y = y;
-            this.x = x;
-            this.count = count;
-        }
-    }
-    
-    private int bfs(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] isVisited = new boolean[n][m];
-        
-        Queue<Point> q = new LinkedList<>();
-        q.add(new Point(0, 0, 1));
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{0, 0, 1});
         isVisited[0][0] = true;
         
         while (!q.isEmpty()) {
-            Point p = q.poll();
-            if (p.y == n - 1 && p.x == m - 1) {
-                return p.count;
+            int[] p = q.poll();
+            if (p[0] == maps.length - 1 && p[1] == maps[0].length - 1) {
+                return p[2];
             }
             
             for (int i = 0; i < 4; i++) {
-                int ny = p.y + d[i][0];
-                int nx = p.x + d[i][1];
+                int ny = p[0] + d[i][0];
+                int nx = p[1] + d[i][1];
                 
-                if (!(0 <= ny && ny < n && 0 <= nx && nx < m)) {
+                if(!(0 <= ny && ny < maps.length && 0 <= nx && nx < maps[0].length)) {
                     continue;
                 }
                 
-                if (maps[ny][nx] == 0 || isVisited[ny][nx]) {
+                if (isVisited[ny][nx] || maps[ny][nx] == 0) {
                     continue;
                 }
                 
+                q.offer(new int[]{ny, nx, p[2] + 1});
                 isVisited[ny][nx] = true;
-                q.add(new Point(ny, nx, p.count + 1));
             }
         }
         
         return -1;
-    }
-    
-    public int solution(int[][] maps) {
-        return bfs(maps);
     }
 }
