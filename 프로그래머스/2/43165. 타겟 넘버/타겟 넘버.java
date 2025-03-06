1b7275f567d277645247d02b61d1d92697e7d164
동팔이 <85945540+Chaos0103@users.noreply.github.com>
2025-03-06 16:46:25 +0900

    [level 2] Title: 타겟 넘버, Time: 7.69 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,22 +1,16 @@
 class Solution {
+    public int solution(int[] numbers, int target) {
+        return dfs(numbers, target, 0, 0);
+    }
     
-    private int target;
-    
-    private int dfs(int[] numbers, int calc, int depth) {
+    private int dfs(int[] numbers, int target, int calc, int depth) {
         if (depth == numbers.length) {
             if (calc == target) {
                 return 1;
             }
             return 0;
         }
-        
-        return dfs(numbers, calc + numbers[depth], depth + 1)
-        + dfs(numbers, calc - numbers[depth], depth + 1);
-    }
-    
-    public int solution(int[] numbers, int target) {
-        this.target = target;
-        
-        return dfs(numbers, 0, 0);
+        return dfs(numbers, target, calc + numbers[depth], depth + 1) 
+            + dfs(numbers, target, calc - numbers[depth], depth + 1);
     }
 }
