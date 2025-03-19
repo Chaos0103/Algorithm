c9d302392d030d7578f7a8cb0adbc0a286faf261
동팔이 <85945540+Chaos0103@users.noreply.github.com>
2025-03-19 22:40:21 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 80.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.86 ms
+메모리: 80.4 MB, 시간: 0.17 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 03월 19일 22:34:54
+2025년 03월 19일 22:40:19
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,25 +1,10 @@
-import java.util.*;
-
 class Solution {
     public int solution(int n, int[][] computers) {
-        List<List<Integer>> network = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            network.add(new ArrayList<>());
-        }
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (i != j && computers[i][j] == 1) {
-                    network.get(i).add(j);
-                }
-            }
-        }
-        
         int answer = 0;
         boolean[] isVisited = new boolean[n];
         for (int i = 0; i < n; i++) {
             if (!isVisited[i]) {
-                bfs(network, isVisited, i);
+                dfs(computers, isVisited, i);
                 answer++;
             }
         }
@@ -27,18 +12,11 @@ class Solution {
         return answer;
     }
     
-    private void bfs(List<List<Integer>> network, boolean[] isVisited, int start) {
-        Queue<Integer> q = new ArrayDeque<>();
-        q.offer(start);
-        isVisited[start] = true;
-        
-        while (!q.isEmpty()) {
-            int now = q.poll();
-            for (int i = 0; i < network.get(now).size(); i++) {
-                if (!isVisited[network.get(now).get(i)]) {
-                    q.offer(network.get(now).get(i));
-                    isVisited[network.get(now).get(i)] = true;
-                }
+    private void dfs(int[][] computers, boolean[] isVisited, int no) {
+        isVisited[no] = true;
+        for (int i = 0; i < computers.length; i++) {
+            if (computers[no][i] == 1 && !isVisited[i]) {
+                dfs(computers, isVisited, i);
             }
         }
     }
