5b17872a7e63b71df8cd4e7e49a45d2e8f4c60c7
동팔이 <85945540+Chaos0103@users.noreply.github.com>
2025-03-19 22:34:56 +0900

    [level 3] Title: 네트워크, Time: 0.86 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,21 +1,16 @@
 import java.util.*;
 
 class Solution {
-    
-    private final ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
-    
     public int solution(int n, int[][] computers) {
+        List<List<Integer>> network = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
+            network.add(new ArrayList<>());
         }
         
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
-                if (computers[i][j] == 1) {
-                    graph.get(i).add(j);
+                if (i != j && computers[i][j] == 1) {
+                    network.get(i).add(j);
                 }
             }
         }
@@ -24,7 +19,7 @@ class Solution {
         boolean[] isVisited = new boolean[n];
         for (int i = 0; i < n; i++) {
             if (!isVisited[i]) {
-                bfs(i, isVisited);    
+                bfs(network, isVisited, i);
                 answer++;
             }
         }
@@ -32,17 +27,17 @@ class Solution {
         return answer;
     }
     
-    private void bfs(int start, boolean[] isVisited) {
+    private void bfs(List<List<Integer>> network, boolean[] isVisited, int start) {
         Queue<Integer> q = new ArrayDeque<>();
         q.offer(start);
         isVisited[start] = true;
+        
         while (!q.isEmpty()) {
-            int node = q.poll();
-            for (int i = 0; i < graph.get(node).size(); i++) {
-                int next = graph.get(node).get(i);
-                if (!isVisited[next]) {
-                    isVisited[next] = true;
-                    q.offer(next);
+            int now = q.poll();
+            for (int i = 0; i < network.get(now).size(); i++) {
+                if (!isVisited[network.get(now).get(i)]) {
+                    q.offer(network.get(now).get(i));
+                    isVisited[network.get(now).get(i)] = true;
                 }
             }
         }
