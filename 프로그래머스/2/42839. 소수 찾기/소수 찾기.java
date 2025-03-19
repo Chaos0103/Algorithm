d32e6bbbc7b640e02479ee3e4b2f48aa52b492cb
동팔이 <85945540+Chaos0103@users.noreply.github.com>
2025-03-19 21:46:43 +0900

    [level 2] Title: 소수 찾기, Time: 100.74 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,46 +1,48 @@
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 
 class Solution {
-    
-    private final Set<Integer> set = new HashSet<>();
-    
+
     public int solution(String numbers) {
+        Set<Integer> set = new HashSet<>();
         for (int len = 1; len <= numbers.length(); len++) {
-            comb(numbers, new boolean[numbers.length()], len, 0, "");    
+            comb(numbers, new boolean[numbers.length()], len, 0, new StringBuilder(), set);
         }
-        
+
         return set.size();
     }
-    
-    private void comb(String numbers, boolean[] isUsed, int len, int depth, String result) {
+
+    private void comb(String numbers, boolean[] isUsed, int len, int depth, StringBuilder sb, Set<Integer> set) {
         if (len == depth) {
-            int num = Integer.parseInt(result);
+            int num = Integer.parseInt(sb.toString());
             if (isPrime(num)) {
                 set.add(num);
             }
             return;
         }
-        
+
         for (int i = 0; i < numbers.length(); i++) {
             if (!isUsed[i]) {
                 isUsed[i] = true;
-                comb(numbers, isUsed, len, depth + 1, result + numbers.substring(i, i + 1));
+                sb.append(numbers.charAt(i));
+                comb(numbers, isUsed, len, depth + 1, sb, set);
+                sb.deleteCharAt(sb.length() - 1);
                 isUsed[i] = false;
             }
         }
     }
-    
+
     private boolean isPrime(int num) {
         if (num < 2) {
             return false;
         }
-        
+
         for (int i = 2; i < num; i++) {
             if (num % i == 0) {
                 return false;
             }
         }
-        
+
         return true;
     }
 }
