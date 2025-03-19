5db3969df87c279fa6539c1f1382b4a1aa0940e3
동팔이 <85945540+Chaos0103@users.noreply.github.com>
2025-03-19 16:49:46 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 12.24 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,54 +1,47 @@
 import java.util.*;
 
 class Solution {
-    private static final List<List<Integer>> graph = new ArrayList<>();
     
-    private int count(int n, int[] remove) {
-        boolean[] isVisited = new boolean[n + 1];
-        
-        Queue<Integer> q = new LinkedList<>();
-        q.add(1);
-        isVisited[1] = true;
-        
-        while (!q.isEmpty()) {
-            int elem = q.poll();
-            for (int i = 0; i < graph.get(elem).size(); i++) {
-                if (isVisited[graph.get(elem).get(i)] ||
-                    (elem == remove[0] && graph.get(elem).get(i) == remove[1]) ||
-                   (elem == remove[1] && graph.get(elem).get(i) == remove[0])) {
-                    continue;
-                }
-                q.add(graph.get(elem).get(i));
-                isVisited[graph.get(elem).get(i)] = true;
-            }
-        }
-        
-        int count = 0;
-        for (int i = 1; i <= n; i++) {
-            if (isVisited[i]) {
-                count++;
-            }
-        }
-        
-        return Math.abs(n - count * 2);
-    }
+    private final List<List<Integer>> network = new ArrayList<>();
     
     public int solution(int n, int[][] wires) {
         for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+            network.add(new ArrayList<>());
         }
         
+        for (int i = 0; i < wires.length; i++) {
+            network.get(wires[i][0]).add(wires[i][1]);
+            network.get(wires[i][1]).add(wires[i][0]);
+        }
+        
+        int answer = n;
         for (int[] wire : wires) {
-            graph.get(wire[0]).add(wire[1]);
-            graph.get(wire[1]).add(wire[0]);
+            Queue<Integer> q = new ArrayDeque<>();
+            boolean[] isVisited = new boolean[n + 1];
+            q.offer(1);
+            isVisited[1] = true;
+            
+            int count = 0;
+            while (!q.isEmpty()) {
+                int no = q.poll();
+                count++;
+                for (int i = 0; i < network.get(no).size(); i++) {
+                    if (isVisited[network.get(no).get(i)]) {
+                        continue;
+                    }
+                    
+                    if ((no == wire[0] && network.get(no).get(i) == wire[1])
+                       || (no == wire[1] && network.get(no).get(i) == wire[0])) {
+                           continue;
+                       }
+                    q.offer(network.get(no).get(i));
+                    isVisited[network.get(no).get(i)] = true;
+                }
+            }
+            
+            answer = Math.min(answer, Math.abs(n - count * 2));
         }
         
-        int result = 1000;
-        for (int[] wire : wires) { 
-            int count = count(n, wire);
-            result = Math.min(result, count);
-        }
-        
-        return result;
+        return answer;
     }
 }
