b15cb3e5e4751fa91e401fab5403e38cf7579591
동팔이 <85945540+Chaos0103@users.noreply.github.com>
2025-03-26 14:34:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 116 ms, Memory: 14344 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,9 +1,12 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
@@ -14,20 +17,34 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int[] dp = new int[n];
+        List<Integer> lis = new ArrayList<>();
+        lis.add(arr[0]);
+
         for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (arr[i] > arr[j]) {
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
-                }
+            if (arr[i] > lis.get(lis.size() - 1)) {
+                lis.add(arr[i]);
+            } else {
+                int index = binarySearch(lis, arr[i]);
+                lis.set(index, arr[i]);
             }
         }
 
-        int max = 0;
-        for (int c : dp) {
-            max = Math.max(max, c);
+        System.out.println(lis.size());
+    }
+
+    private static int binarySearch(List<Integer> arr, int target) {
+        int left = 0;
+        int right = arr.size();
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (arr.get(mid) < target) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
         }
 
-        System.out.println(max + 1);
+        return left;
     }
 }
