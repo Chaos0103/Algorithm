9d294d9844a00331feb62da290ac82435a550fd6
동팔이🍊 <85945540+Chaos0103@users.noreply.github.com>
2025-10-27 16:53:31 +0900

    [level 1] Title: 폰켓몬, Time: 4.52 ms, Memory: 96.5 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -1,16 +1,12 @@
 import java.util.*;
 
 class Solution {
-    //총 N마리중 N/2마리 가져갈 수 있음
-    //같은 종류의 폰켓몬은 같은 번호
-    //가장 많은 종류의 폰켓몬을 선택하는 방법을 찾아 갯수를 반환
     public int solution(int[] nums) {
         Set<Integer> set = new HashSet<>();
         for (int num : nums) {
             set.add(num);
         }
-    
-        int half = nums.length / 2;
-        return set.size() < half ? set.size() : half;
+        int n = nums.length / 2;
+        return n > set.size() ? set.size() : n;
     }
 }
