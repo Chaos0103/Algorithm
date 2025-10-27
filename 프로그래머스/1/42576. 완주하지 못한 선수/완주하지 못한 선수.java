32df1f1bce486b750d5bb0b4ac6a52ea610ffa28
동팔이🍊 <85945540+Chaos0103@users.noreply.github.com>
2025-10-27 15:25:21 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 76.00 ms, Memory: 97.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,13 +4,12 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> map = new HashMap<>();
         for (String name : completion) {
-            int count = map.getOrDefault(name, 0);
-            map.put(name, count + 1);
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
         for (String name : participant) {
             int count = map.getOrDefault(name, 0);
-            if (count <= 0) {
+            if (count == 0) {
                 return name;
             }
             map.put(name, count - 1);
